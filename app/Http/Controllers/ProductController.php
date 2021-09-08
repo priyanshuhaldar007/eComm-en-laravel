a64d57755c44c3e3944c3752726c2e2c40bb9528
priyanshuhaldar007 <priyanshu1843@gmail.com>
2021-09-08 19:22:09 +0530

    logout login done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
-
+use Session;
 class ProductController extends Controller
 {
     //
@@ -35,5 +35,9 @@ class ProductController extends Controller
         else{
             return redirect('/login');
         }
-     }
+    }
+    static function cartItem(){
+        $userId=Session::get('user')['id'];
+        return Cart::where('user_id',$userId)->count();
+    }
 }
